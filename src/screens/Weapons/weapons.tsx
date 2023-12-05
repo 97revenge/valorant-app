96ca06cd48f3b8b9cf75391158f0ed56a167b10b
97revenge <empregos97@gmail.com>
2023-12-05 18:44:40 -0300

    feature:Added horizontal ScrollView with styled data elements for item details.

--- src/screens/Weapons/weapons.tsx
+++ src/screens/Weapons/weapons.tsx
@@ -1,26 +1,37 @@
 import axios, { AxiosResponse, AxiosError } from 'axios';
+import { useNavigation } from '@react-navigation/native';
 
 import { LinearGradient } from 'expo-linear-gradient';
 
-import { ViewComponent, Text, View, ScrollView } from 'react-native';
+import {
+  ViewComponent,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 import CoreTitle from '../../components/Headers/CoreTitle';
 import { useState, useEffect, useCallback } from 'react';
+import { HabilitieIcon } from '../../components/Icons/HabilitieIcon';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../navigation';
 
 export default function Weapons() {
   const [load, setLoad] = useState<Array<any>>([]);
-
-  const fetch = useCallback(async () => {
-    await axios
-      .get('https://valorant-api.com/v1/weapons')
-      .then((response: AxiosResponse) => {
-        setLoad(response.data);
-        alert(load);
-      })
-      .catch((err: AxiosError) => alert(err.status));
-  }, []);
+  type OverviewScreenNavigationProps = StackNavigationProp<RootStackParamList, 'Overview'>;
+  const navigation = useNavigation<OverviewScreenNavigationProps>();
 
   useEffect(() => {
-    fetch();
+    axios
+      .get('https://valorant-api.com/v1/weapons')
+      .then((response) => {
+        const { data } = response.data;
+        setLoad(data);
+      })
+      .catch((error) => alert(error));
   }, []);
 
   return (
@@ -32,7 +43,38 @@ export default function Weapons() {
       <CoreTitle title="Weapons" />
       <ScrollView>
         <View className=" w-full h-full rounded-lg p-8 bg-[#2022253f]     ">
-          <Text>Ok</Text>
+          {load.map((item) => {
+            return (
+              <SafeAreaView
+                key={item.uuid}
+                className="rounded-b-lg mb-5 bg-[#ffffff18]  border-t-8  border-t-[#ffffff3f]   shadow-lg ">
+                <TouchableOpacity className="">
+                  <View className="flex items-start my-4 w-full p-2">
+                    <Text className="font-valorant text-white text-4xl text-start">
+                      {item.displayName}
+                    </Text>
+                    <View className="w-full h-auto  px-2">
+                      <Image
+                        source={{ uri: item.displayIcon }}
+                        className="object-cover  w-[350px] h-[120px]     "
+                      />
+                    </View>
+                  </View>
+                </TouchableOpacity>
+                <ScrollView horizontal={true} className=" w-auto h-auto gap-2  py-2 mx-2 ">
+                  <View className="bg-red-200 rounded-lg px-5 ">
+                    <Text>Cost Rate: {item.shopData?.cost}</Text>
+                  </View>
+                  <View className="bg-red-200 rounded-lg px-5">
+                    <Text> {item.shopData?.category}</Text>
+                  </View>
+                  <View className="bg-red-200 rounded-lg px-5">
+                    <Text>Fire Rate: {item.weaponStats?.fireRate}</Text>
+                  </View>
+                </ScrollView>
+              </SafeAreaView>
+            );
+          })}
         </View>
       </ScrollView>
     </LinearGradient>
