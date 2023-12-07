2b6f8087388acdac373c2b9915b0ff2cb55a7fde
97revenge <empregos97@gmail.com>
2023-12-07 16:33:54 -0300

    refact:weapon details component with dynamic rates with namespace

--- src/components/Sections/RateWeaponsSection.tsx
+++ src/components/Sections/RateWeaponsSection.tsx
@@ -1,4 +1,5 @@
 import { ScrollView, View, Text } from 'react-native';
+import { W, wx } from 'windstitch';
 
 type Props = {
   cost: undefined;
@@ -7,18 +8,36 @@ type Props = {
   magazineSize: undefined;
 };
 
-type Elem = React.ReactNode;
+namespace WeaponSection {
+  type Elem = React.ReactNode;
 
-const Cost = ({ children = <View></View> }: { children: Elem }): Elem => {
-  return <View>{children}</View>;
-};
+  export const Cost = ({ children = <View></View> }: { children: Elem }): Elem => {
+    return <View>{children}</View>;
+  };
+  export const Category = ({ children = <View></View> }: { children: Elem }): Elem => {
+    return <View>{children}</View>;
+  };
+  export const FireRate = ({ children = <View></View> }: { children: Elem }): Elem => {
+    return <View>{children}</View>;
+  };
+  export const MagazineSize = ({ children = <View></View> }: { children: Elem }): Elem => {
+    return <View>{children}</View>;
+  };
+}
 
 export const RateWeaponsSection = ({ cost, category, fireRate, magazineSize }: Props) => {
   return (
     <ScrollView horizontal={true} className="gap-2 w-auto px-4 py-2 rounded-lg ">
-      <View className="bg-[#ffffff31] w-auto p-2 rounded-lg shadow-lg ">
-        <Text className="font-bold text-white ">Cost Rate: {cost}</Text>
-      </View>
+      <WeaponSection.Cost>
+        {typeof cost === 'number' && (
+          <View
+            className={
+              typeof cost === 'number' ? 'bg-[#ffffff31] w-auto p-2 rounded-lg shadow-lg mt-2' : ''
+            }>
+            <Text className="font-bold text-white ">Cost Rate: {cost} </Text>
+          </View>
+        )}
+      </WeaponSection.Cost>
       <View className="bg-[#ffffff31] w-auto p-2 rounded-lg shadow-lg">
         <Text className="font-bold text-white "> {category}</Text>
       </View>
