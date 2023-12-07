349d80c0669d1b6bc76420b55219906bcbb98d9c
97revenge <empregos97@gmail.com>
2023-12-07 17:28:36 -0300

    feature:Introducing a flexible styling concept with Windstitch, defining primary views and texts dynamically with key in sintax in typescript

--- src/components/Sections/RateWeaponsSection.tsx
+++ src/components/Sections/RateWeaponsSection.tsx
@@ -1,11 +1,31 @@
 import { ScrollView, View, Text } from 'react-native';
 import { W, wx } from 'windstitch';
 
-type Props = {
-  cost: undefined;
-  category: undefined;
-  fireRate: undefined;
-  magazineSize: undefined;
+const concept = wx({
+  variants: {
+    primary: {
+      view: `bg-[#ffffff31] w-auto p-2 rounded-lg shadow-lg mt-2 mr-1`,
+      text: `font-bold text-white`,
+    },
+  },
+});
+
+type Concept<T> = {
+  [key in keyof T]: W.Infer<typeof concept>;
+};
+
+type Tag = { view: void } | { text: void };
+
+const primaryView: Concept<Tag> = {
+  view: concept({
+    primary: 'view',
+  }),
+};
+
+const primaryText: Concept<Tag> = {
+  view: concept({
+    primary: 'text',
+  }),
 };
 
 namespace WeaponSection {
@@ -25,55 +45,48 @@ namespace WeaponSection {
   };
 }
 
-export const RateWeaponsSection = ({ cost, category, fireRate, magazineSize }: Props) => {
+type Props = {
+  cost: undefined;
+  category: undefined;
+  fireRate: undefined;
+  magazineSize: undefined;
+};
+
+type New<T> = {
+  [key in keyof T]: T;
+};
+
+export const RateWeaponsSection = ({ cost, category, fireRate, magazineSize }: New<Props>) => {
   return (
-    <ScrollView horizontal={true} className="gap-2 w-auto px-4 py-2 rounded-lg ">
+    <ScrollView horizontal={true} className="gap-2 w-auto px-4 py-2 rounded-lg   ">
       <WeaponSection.Cost>
         {typeof cost === 'number' && (
-          <View
-            className={
-              typeof cost === 'number' ? 'bg-[#ffffff31] w-auto p-2 rounded-lg shadow-lg mt-2' : ''
-            }>
-            <Text className="font-bold text-white ">Cost Rate: {cost} </Text>
+          <View className={typeof cost === 'number' ? (primaryView.view as string) : ''}>
+            <Text className={primaryText.view as string}>Cost Rate: {cost} </Text>
           </View>
         )}
       </WeaponSection.Cost>
 
       <WeaponSection.Category>
         {typeof category === 'string' && (
-          <View
-            className={
-              typeof category === 'string'
-                ? 'bg-[#ffffff31] w-auto p-2 rounded-lg shadow-lg mx-1 mt-2'
-                : ''
-            }>
-            <Text className="font-bold text-white "> {category} </Text>
+          <View className={typeof category === 'string' ? (primaryView.view as string) : ''}>
+            <Text className={primaryText.view as string}> {category} </Text>
           </View>
         )}
       </WeaponSection.Category>
 
       <WeaponSection.FireRate>
         {typeof fireRate === 'number' && (
-          <View
-            className={
-              typeof category === 'string'
-                ? 'bg-[#ffffff31] w-auto p-2 rounded-lg shadow-lg  mt-2'
-                : ''
-            }>
-            <Text className="font-bold text-white ">Fire Rate: {fireRate} </Text>
+          <View className={typeof category === 'string' ? (primaryView.view as string) : ''}>
+            <Text className={primaryText.view as string}>Fire Rate: {fireRate} </Text>
           </View>
         )}
       </WeaponSection.FireRate>
 
       <WeaponSection.MagazineSize>
         {typeof magazineSize === 'number' && (
-          <View
-            className={
-              typeof category === 'string'
-                ? 'bg-[#ffffff31] w-auto p-2 rounded-lg shadow-lg  mt-2 mx-1'
-                : ''
-            }>
-            <Text className="font-bold text-white ">Magazine Size: {magazineSize} </Text>
+          <View className={typeof category === 'string' ? (primaryView.view as string) : ''}>
+            <Text className={primaryText.view as string}>Magazine Size: {magazineSize} </Text>
           </View>
         )}
       </WeaponSection.MagazineSize>
